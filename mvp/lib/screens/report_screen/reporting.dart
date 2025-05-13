38c62f285f0e410838e805446d12e28ab0b36461
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-13 23:55:00 +1000

    Added navigation bar & titles for reporting

--- mvp/lib/screens/report_screen/reporting.dart
+++ mvp/lib/screens/report_screen/reporting.dart
@@ -12,15 +12,86 @@ class ReportingScreen extends StatefulWidget {
 class _ReportingScreen extends State<ReportingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          "Report Damage",
-          style: TextStyle(
-            color: Color(0xFF327da8),
+    return Scaffold(
+      //--- Language button ---
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.language),
+            iconSize: 26,
           ),
+        ],
+      ),
+      //--- Information ---
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //-- Title --
+            Text(
+              "Report Damage",
+              style: TextStyle(
+                color: Color(0xFF353B3C),
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            //-- Damage type --
+            Text(
+              "Type of Damage",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+
+            //-- Location --
+            Text(
+              "Damage Location",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+
+            //-- Photo --
+            Text(
+              "Provide a photo of the damage",
+              style: TextStyle(fontSize: 18),
+            ),
+
+            //-- Additional info --
+            Text(
+              "Additional information or comments",
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
         ),
-      ],
+      ),
+      //--- Navigation Bar ---
+      //-- Reporting --
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 59, 152, 198),
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          //-- Home --
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          //-- Report History --
+          NavigationDestination(
+            icon: Icon(Icons.history),
+            label: 'Report History',
+          ),
+        ],
+        indicatorColor: const Color.fromARGB(255, 59, 152, 198),
+      ),
     );
   }
 }
