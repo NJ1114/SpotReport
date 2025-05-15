0404cc83324bc24e3c2591e918492e153e840233
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-16 00:46:39 +1000

    Moved navigation bar into a separate widget

---
+++ mvp/lib/widgets/navigation_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:mvp/screens/report_screen/report_history.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomNavBar();
+  }
+}
+
+class _BottomNavBar extends State<BottomNavBar> {
+  final List<Widget> screens = [
+    ReportHistory(),
+  ];
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentPageIndex],
+      //-- Reporting --
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFF3B98C6),
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: [
+          //-- Home --
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          //-- Report History --
+          NavigationDestination(
+            icon: Icon(Icons.history),
+            label: 'Report History',
+          ),
+        ],
+        indicatorColor: const Color(0xFF3B98C6),
+      ),
+    );
+  }
+}
