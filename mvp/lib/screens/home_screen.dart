1c5524c63bfb1d69d0c04f0b7b314921224adcfc
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-16 14:08:26 +1000

    Created homescreen as placeholder

---
+++ mvp/lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //--- Language button ---
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.language),
+            iconSize: 26,
+          ),
+        ],
+      ),
+      //--- Report History Main ---
+      body: Center(child: Text("Home Screen Placeholder")),
+    );
+  }
+}
