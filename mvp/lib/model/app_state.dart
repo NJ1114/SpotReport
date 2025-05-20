4cf5dce36ac3ee643cee15edde1b5206e3d50bb7
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-20 18:09:33 +1000

    Created base app state with add report

--- mvp/lib/model/app_state.dart
+++ mvp/lib/model/app_state.dart
@@ -1,1 +1,24 @@
-import 'package:flutter/material.dart';
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:mvp/model/report_model.dart';
+
+class AppState extends ChangeNotifier {
+  //All reports submitted
+  final List<Report> _mySubmissions = [
+    //tester submission
+    Report(
+        damage: DamageType.sign,
+        location: "Epping",
+        info: "Car crashed into the sign"),
+  ];
+
+  //-- Unmodifiable list view of user's submissions
+  UnmodifiableListView<Report> get submissions => UnmodifiableListView(_mySubmissions);
+
+  //-- Add report to full list --
+  void add(Report report) {
+    _mySubmissions.add(report);
+    notifyListeners();
+  }
+}
