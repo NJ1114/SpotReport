564b08be589ce243c21c09b6dd1945dfac441b68
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-20 23:57:02 +1000

    Implemented firebase initialisation & moved material app

--- mvp/lib/main.dart
+++ mvp/lib/main.dart
@@ -1,13 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mvp/model/app_state.dart';
+import 'package:mvp/screens/login_screens/login_register.dart';
 import 'package:mvp/spotreport.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppState(),
-      child: const SpotReport(),
+      child: MaterialApp(
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              User? user = snapshot.data;
+              if (user != null) {
+                return const SpotReport();
+              } else {
+                return const UserLogRegScreen();
+              }
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
     ),
   );
 }
