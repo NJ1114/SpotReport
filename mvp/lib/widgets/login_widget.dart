941c2b4159275ec862e80f2b8fa5e2a6836f91b7
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-20 14:21:28 +1000

    Added login button, register text and controllers, fixed style of textfield

--- mvp/lib/widgets/login_widget.dart
+++ mvp/lib/widgets/login_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginWidget extends StatefulWidget {
@@ -10,33 +11,95 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidget extends State<LoginWidget> {
-  
+  //--- Controllers ---
+  final _loginEmailControl = TextEditingController();
+  final _loginPassControl = TextEditingController();
 
+  @override
+  void dispose() {
+    _loginEmailControl.dispose();
+    _loginPassControl.dispose();
+    super.dispose();
+  }
+
+  //--- Login Handling ---
+  Future<void> handleLogin() async {}
 
   @override
   Widget build(BuildContext context) {
+    //--- Login Design ---
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        //--- Email Input ---
+        //-- Email Input --
         TextField(
           decoration: InputDecoration(
-            border: OutlineInputBorder(),
             label: Text("Enter your email"),
+            border: OutlineInputBorder(),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFF3B98C6),
+              ),
+            ),
           ),
         ),
         SizedBox(
           height: 30,
         ),
-        //--- Password Input ---
+        //-- Password Input --
         TextField(
           decoration: InputDecoration(
-            border: OutlineInputBorder(),
             label: Text("Enter your password"),
+            border: OutlineInputBorder(),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFF3B98C6),
+              ),
+            ),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 70,
+        ),
+        Center(
+          child: RichText(
+            text: TextSpan(
+              text: "Don't have an account? ",
+              style: TextStyle(
+                fontSize: 16,
+                color: Color(0xFF353B3C),
+              ),
+              children: [
+                TextSpan(
+                  text: "Register",
+                  style: TextStyle(
+                    color: Color(0xFF3B98C6),
+                  ),
+                  // recognizer: TapGestureRecognizer()..onTap() {Navigator.push(context, route)},
+                )
+              ],
+            ),
           ),
         ),
         SizedBox(
-          height: 50,
+          height: 15,
         ),
+        ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF3B98C6),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ))
       ],
     );
   }
