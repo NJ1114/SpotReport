837d608a934a589f576681db2b76c708740d711a
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-21 11:58:07 +1000

    Created custom theme schemes for light and dark mode

---
+++ mvp/lib/model/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+final myLightTheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF3B98C6),
+  onPrimary: Color(0xFFEEF0F2),
+  secondary: Color(0xFF846A6A),
+  onSecondary: Color(0xFFFFFFFF),
+  error: Color(0xFFE57373),
+  onError: Color(0xFFFFFFFF),
+  surface: Color(0xFFEEF0F2),
+  onSurface: Color(0xFF8E9193),
+);
+
+final myDarkTheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF3B98C6),
+  onPrimary: Color(0xFFEEF0F2),
+  secondary: Color(0xFF846A6A),
+  onSecondary: Color(0xFFFFFFFF),
+  error: Color(0xFFE57373),
+  onError: Color(0xFFFFFFFF),
+  surface: Color(0xFFEEF0F2),
+  onSurface: Color(0xFF8E9193),
+);
