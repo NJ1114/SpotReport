f76f29b30a3c2c7ceed7a634a3f4192484bada6c
Vanessa Poon <vanessapa2013@hotmail.com>
2025-05-21 23:16:48 +1000

    Initialised light theme

--- mvp/lib/main.dart
+++ mvp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mvp/model/app_state.dart';
+import 'package:mvp/model/theme.dart';
 import 'package:mvp/screens/login_screens/login_register.dart';
 import 'package:mvp/spotreport.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  //--- Firebase initialiser ---
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -17,6 +19,12 @@ Future<void> main() async {
     ChangeNotifierProvider(
       create: (context) => AppState(),
       child: MaterialApp(
+        //--- Light Theme ---
+        theme: myLightThemeScheme,
+        
+        //--- Dark Theme ---
+        
+        //--- App Functions/Opening ---
         home: StreamBuilder<User?>(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
