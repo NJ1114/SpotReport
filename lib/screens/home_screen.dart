644175bb438f075099a8869984efa3c4a71f7298
Vanessa Poon <vanessapa2013@hotmail.com>
2025-07-31 19:26:01 +1000

    Changed class to stateful widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spotreport/model/app_state.dart';
+import 'package:spotreport/widgets/report_list.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  Future<void> getRecentReports() async {
+    final allReports =
+        await Provider.of<AppState>(context, listen: false).getReport();
+    // final recentReports = allReports.length > 2
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //--- Report History Main ---
-      body: Center(
-        child: Text(
-          "Home Screen Placeholder",
-          style: Theme.of(context).textTheme.titleMedium,
-        ),
-      ),
     );
   }
 }
