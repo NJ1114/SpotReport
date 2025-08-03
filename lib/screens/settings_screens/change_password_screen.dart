8f54c0bd0a0f490092ddf84a5af1eda97d556c8c
Vanessa Poon <vanessapa2013@hotmail.com>
2025-08-03 16:49:17 +1000

    Added appbar & screen heading

--- lib/screens/settings_screens/change_password_screen.dart
+++ lib/screens/settings_screens/change_password_screen.dart
@@ -4,18 +4,56 @@ class ChangePassword extends StatefulWidget {
   const ChangePassword({super.key});
 
   @override
-  State<StatefulWidget> createState() => _ChangePassword();
+  State<ChangePassword> createState() => _ChangePassword();
 }
 
 class _ChangePassword extends State<ChangePassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        //-- App Logo ---
+        title: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: "Sp",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 18,
+                ),
+              ),
+              TextSpan(
+                text: "o",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 18,
+                ),
+              ),
+              TextSpan(
+                text: "tReport",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 18,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      //-- Main password textfields --
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25.0,
+              vertical: 15.0,
+            ),
+            child: Text(
+              "Change Password",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ),
         ],
       ),
